1134a817116940cb4e8c4ed167395b49907fa2cc
suhellynaraujo <github.interface@gmail.com>
2024-01-17 21:53:03 -0300

    Onde está o bug? correçãogit add . !

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,6 +3,9 @@ let senhaDoSistema = "senhaTeste!";
 
 let senha = prompt("Digite a senha do sistema:");
 
-if (senha = senhaDoSistema){
+if (senha == senhaDoSistema){
     alert("Acesso ao sistema garantido");
+}
+else{
+    alert("Acesso ao sistema negado");
 }
