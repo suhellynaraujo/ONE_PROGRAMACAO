55753ee113fb9d98c09288db8ec25d3ce3ea7020
suhellynaraujo <github.interface@gmail.com>
2024-01-17 21:46:29 -0300

    chute

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,5 +2,9 @@ alert('Boas vindas ao jogo do nnúmero secreto!');
 
 let numeroSecreto = 5;
 
-prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 1 e 10');
 
+if(chute == numeroSecreto){
+
+    console.log('Isso ai! Você descobriu o número secreto (5)');
+}
