a69eb49935af678f9ae4f2285691dce6ec754702
suhellynaraujo <github.interface@gmail.com>
2024-01-19 20:38:31 -0300

    app.js

---
+++ Desafio-hora-da-pratica/desafioNumeroDigitado/app.js
@@ -0,0 +1,7 @@
+let numeroDigitado = prompt("Digite um numero negativo ou positivo");
+
+if(numeroDigitado > 0){
+    alert("O número:" + numeroDigitado + " é positivo!");
+}else{
+    alert("O número:" + numeroDigitado + " é negativo!");
+}
