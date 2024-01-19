b97c6a3d0d69925ccce8304612577d20bfcd28c5
suhellynaraujo <github.interface@gmail.com>
2024-01-19 18:45:23 -0300

    else

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,15 @@
-alert('Boas vindas ao jogo do nnúmero secreto!');
+alert('Boas vindas ao jogo do número secreto');
 
 let numeroSecreto = 5;
 
+console.log('numeroSecreto')
+
 let chute = prompt('Escolha um número entre 1 e 10');
 
-if(chute == numeroSecreto){
-
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+// se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert('Isso ai! Você descobriu o número secreto (5)');
+} else {
+    alert('Você errou :(')
 }
+
