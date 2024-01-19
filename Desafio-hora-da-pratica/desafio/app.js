0c09f47fa9636bdad360421a0841dbe9525fa55f
suhellynaraujo <github.interface@gmail.com>
2024-01-19 19:49:43 -0300

    melhorando a lógica

--- Desafio-hora-da-pratica/desafio/app.js
+++ Desafio-hora-da-pratica/desafio/app.js
@@ -1,29 +1,29 @@
 
 alert('Boas boas-vindas ao nosso site!')
-let nome = 'Lua';
-let idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponível = 1000;
+//let nome = 'Lua';
+//let idade = 25;
+//let numeroDeVendas = 50;
+//let saldoDisponível = 1000;
+
+let nome = prompt('Qual seu nome?');
+let idade = prompt('Digite sua idade: ');
 let mensagemDeErro = 'Erro! Preencha todos os campos.';
 
-nome = prompt('Qual seu nome?');
-idade = prompt('Digite sua idade: ');
-
-if(nome == "" || idade == ""){
+if(nome === "" || idade === ""){
     
     alert(mensagemDeErro)
 
     nome = prompt('Qual seu nome?');
     idade = prompt('Digite sua idade: ');
 
-    if(nome == !null || idade == !null){
+}
+if(nome !== null && idade !== null){
 
-        if(idade <= 18){
-            alert('Pode tirar a habilitação!');        
-        }
-        else{
-            alert('Menor de 18 anos, não está apto(a)!');
-        }
+    if(idade >= 18){
+        alert('Pode tirar a habilitação!');        
+    }
+    else{
+        alert('Menor de 18 anos, não está apto(a)!');
     }
 }
 
