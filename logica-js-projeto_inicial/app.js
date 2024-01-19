8e03c09c06bf19534a4218e3450cd4996858dc5a
suhellynaraujo <github.interface@gmail.com>
2024-01-19 20:11:02 -0300

    renomeando

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,14 +1,15 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 5;
+let numeroSecreto = 9;
 
-console.log('numeroSecreto')
+console.log(numeroSecreto)
 
 let chute = prompt('Escolha um número entre 1 e 10');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}
+    `);
 } else {
     alert('Você errou :(')
 }
