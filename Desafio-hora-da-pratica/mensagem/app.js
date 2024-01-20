cab43c93cb83ba6cdff943a39cea5b6c76ac9931
suhellynaraujo <github.interface@gmail.com>
2024-01-19 21:36:52 -0300

    app.js

---
+++ Desafio-hora-da-pratica/mensagem/app.js
@@ -0,0 +1,9 @@
+
+alert("Bem vindo a sua conta!")
+let resposta = prompt("Deseja consultar seu saldo? S/N");
+let saldo =100;
+if(resposta === "S"){
+alert(`Seu saldo atual é de: ${saldo}$`);
+}else{
+    alert("Até breve");
+}
