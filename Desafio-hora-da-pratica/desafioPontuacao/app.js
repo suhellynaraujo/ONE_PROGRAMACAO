dcc70880b435d8cef1d9974cb6daa36e875285c9
suhellynaraujo <github.interface@gmail.com>
2024-01-19 21:25:55 -0300

    app.js

---
+++ Desafio-hora-da-pratica/desafioPontuacao/app.js
@@ -0,0 +1,12 @@
+alert("Vamos jogar?");
+alert("você pode escolher de 1 a 100!");
+let valor = prompt("Escolha um número: ");
+
+if(valor >=100) {
+     alert("Parabéns, você venceu!");
+ } else {
+     alert("Não foi dessa vez, tente novamente!"); 
+ }
+
+
+
