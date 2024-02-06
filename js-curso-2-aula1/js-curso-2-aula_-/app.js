daae350ccdb72d5c97e8f69d07e7dd6354e2c5c0
suhellynaraujo <github.interface@gmail.com>
2024-02-05 21:58:34 -0300

    querySelector

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do numero sercreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
