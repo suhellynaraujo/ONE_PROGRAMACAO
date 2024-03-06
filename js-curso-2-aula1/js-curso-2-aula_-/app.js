1e3de0d85b702ab5532744fd95947c52c297227e
suhellynaraujo <github.interface@gmail.com>
2024-03-06 20:13:28 -0300

    responsivevoice, não funcionou

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -6,6 +6,8 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female',
+    {rate:1.2});
  }
 
  function exibirMensagemInicial(){
